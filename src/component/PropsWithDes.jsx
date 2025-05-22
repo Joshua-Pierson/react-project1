1d3003cdfe5d3091c1d9441ea31b9b5eb06d6e82
Joshua Pierson <pierson.joshua726@gmail.com>
2025-05-21 19:03:09 -0500

    update

--- src/component/PropsWithDes.jsx
+++ src/component/PropsWithDes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function PropsWithDes() {
+export default function PropsWithDes({studentName, studentlastName}) {
   return (
     <h1>Welcome to class {studentName} {studentlastName} !!</h1>
   )
